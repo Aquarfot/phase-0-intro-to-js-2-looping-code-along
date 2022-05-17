7d0927328445ee1cd58dacc1d3f6a5707acd9b3e
Andrew Quarfot <andrew.quarfot@gmail.com>
2022-05-17 15:10:17 -0600

    commit tree ddcbb580c8d6088868b228371d6f43a3b6e67d86

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let newArray = []
+    for(let i = 0; i < names.length; i++) {
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+}
+return newArray;
+}
+
+function countDown(startingNum) {
+    while (startingNum > 0) {
+        console.log(startingNum);
+        startingNum -= 1;
+    }
+    console.log(startingNum);
+}
